9d2ce85e999e9f00bcd540324c7017ff8eb07a89
kursivee <kursivee.dev@gmail.com>
2020-02-23 12:02:31 -0500

    [material] Add forgot cred bottom sheet

--- material/app/src/main/java/kursivee/com/material/MainActivity.kt
+++ material/app/src/main/java/kursivee/com/material/MainActivity.kt
@@ -1,12 +1,22 @@
 package kursivee.com.material
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_forgot.setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+            val sheet = layoutInflater.inflate(R.layout.bottom_sheet_main, null)
+            dialog.setContentView(sheet)
+            dialog.show()
+        }
     }
 }
