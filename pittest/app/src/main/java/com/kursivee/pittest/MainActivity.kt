f1032ef2a22c5372835dd0bc8062d61373a4a609
kursivee <kursivee.dev@gmail.com>
2020-03-07 09:02:03 -0500

    [pit-test] Initial commit

---
+++ pittest/app/src/main/java/com/kursivee/pittest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kursivee.pittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kursivee.pittest.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
